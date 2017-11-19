2e0c4cced2c61a41da4838d8f15cc2eedfc47c17
vksantoshsunny <vksantoshsunny@gmail.com>
2017-11-19 18:34:36 +1300

    added changes

---
+++ Repository/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Repository.Repository
+{
+	public class ProductRepository : Repository<Product>, IProductRepository
+	{
+
+		public ProductRepository(ApplicationContext context)
+			: base(context)
+		{
+
+		}
+		
+
+		public List<Product> Search(string name)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Repository/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+	public class UnitOfWork: IUnitOfWork
+	{
+		private readonly ApplicationContext _context;
+
+		public UnitOfWork(ApplicationContext context)
+		{
+			_context = context;
+			ProductRepository = new ProductRepository(_context);
+
+		}
+
+		public IProductRepository ProductRepository { get; private set; }
+
+		public int Complete()
+		{
+			return _context.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+
+	}
+}
